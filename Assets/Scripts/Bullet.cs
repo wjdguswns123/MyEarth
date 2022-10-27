d4ac89b64edd6e64f13d9a34861f057411bb78bf
wjdguswns123 <wjdguswns123@hanmail.net>
2022-10-27 19:06:29 +0900

    Bullet 수정

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,42 +3,39 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour, InteractiveObject
 {
-    Move         mover;
-    InfoWeapon   weaponInfo;
+    private Move _mover;
+    private InfoWeapon _weaponInfo;
 
-    float        liveTimer = 0f;
-    float        attackRateTimer = 0f;
-    int          weaponLevel;
+    private float _liveTimer;
+    private float _attackRateTimer;
+    private int _weaponLevel;
 
-    System.Action destroyCallback = null;
-    public System.Action DestroyCallback { set { destroyCallback = value; } }
-
-    //총알 이동 방식 설정.
-    public bool Init(InfoWeapon info, int level)
+    /// <summary>
+    /// 총알 이동 방식 설정.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="level"></param>
+    /// <returns></returns>
+    public void Init(InfoWeapon info, int level)
     {
         switch(info.moveType)
         {
             case Def.DefEnum.MoveType.LINEAR:
-                mover = new LinearMove(transform, info.speed);
+                _mover = new LinearMove(transform, info.speed);
                 break;
             case Def.DefEnum.MoveType.GUIDED:
                 {
                     var destEnemy = BattleManager.Instance.SearchNearEnemy(transform, info.range);
                     var dest = destEnemy != null ? destEnemy.transform : null;
-                    mover = new GuidedMove(transform, dest, info.speed, info.range);
+                    _mover = new GuidedMove(transform, dest, info.speed, info.range);
                 }
                 break;
         }
-        weaponInfo = info;
-        weaponLevel = level;
-        Refresh();
+        _weaponInfo = info;
+        _weaponLevel = level;
 
-        return weaponInfo.speed == 0 && weaponInfo.lifeTime > 0f;
-    }
-
-    public void Refresh()
-    {
-        liveTimer = 0f;
+        _liveTimer = 0f;
+        _attackRateTimer = 0f;
     }
 
     // Update is called once per frame
@@ -46,68 +43,98 @@ public class Bullet : MonoBehaviour, InteractiveObject
     {
         if (BattleManager.Instance.GameState == Def.DefEnum.GameState.PLAY)
         {
-            mover.Moving();
+            _mover.Moving();
 
             //일정 시간 후 삭제.
-            if (liveTimer >= GetLifeTime())
+            if (_liveTimer >= GetLifeTime())
             {
                 Destroy();
             }
             else
             {
-                liveTimer += Time.deltaTime;
+                _liveTimer += Time.deltaTime;
             }
 
             //총알이 콜라이더를 가지고 있지 않으면 일정 시간 후 범위 내 적 검사.
-            if (!weaponInfo.haveCollider && (attackRateTimer >= 0.2f))
+            if (!_weaponInfo.haveCollider && (_attackRateTimer >= 0.2f))
             {
                 CheckInRange();
-                attackRateTimer = 0;
+                _attackRateTimer = 0;
             }
             else
             {
-                attackRateTimer += Time.deltaTime;
+                _attackRateTimer += Time.deltaTime;
             }
-        }
-        else if (BattleManager.Instance.GameState == Def.DefEnum.GameState.END)
-        {
-            //게임 종료되면 탄환 삭제.
-            //Destroy(gameObject);
-            //ResourceManager.Instance.DestroyObject(weaponInfo.bulletPath, gameObject);
-            gameObject.SetActive(false);
         }
     }
 
-    //충돌 처리.
+    /// <summary>
+    /// 유지 시간 계산.
+    /// </summary>
+    /// <returns></returns>
+    private float GetLifeTime()
+    {
+        float lifeTime = _weaponInfo.lifeTime + _weaponInfo.lifeTimeUpg * _weaponLevel;
+        return lifeTime <= _weaponInfo.lifeTimeMax ? lifeTime : _weaponInfo.lifeTimeMax;
+    }
+
+    /// <summary>
+    /// 범위 내 적 공격.
+    /// </summary>
+    private void CheckInRange()
+    {
+        if (_weaponInfo.attackType == Def.DefEnum.AttackType.ANGLE_RANGE)
+        {
+            BattleManager.Instance.AngleRangeAttack(transform, _weaponInfo.length, _weaponInfo.range, GetAttackValue());
+        }
+        else if (_weaponInfo.attackType == Def.DefEnum.AttackType.FRONT_RANGE)
+        {
+            //범위 공격에 맞은 적이 있으면 사운드 출력.
+            if (BattleManager.Instance.FrontRangeAttack(transform, _weaponInfo.range, _weaponInfo.length, GetAttackValue()))
+            {
+                SoundManager.Instance.PlaySound(_weaponInfo.hitSFXPath);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 충돌 처리.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         Destroy();
-        if (weaponInfo.attackType == Def.DefEnum.AttackType.SINGLE)
+        if (_weaponInfo.attackType == Def.DefEnum.AttackType.SINGLE)
         {
             InteractiveObject obj = other.GetComponent<InteractiveObject>();
             if (obj != null)
             {
                 obj.Attacked(GetAttackValue());
-                SoundManager.Instance.PlaySound(weaponInfo.hitSFXPath);
+                SoundManager.Instance.PlaySound(_weaponInfo.hitSFXPath);
             }
         }
     }
 
-    //범위 내 적 공격.
-    void CheckInRange()
+    /// <summary>
+    /// 자신 해제.
+    /// </summary>
+    public void Destroy()
     {
-        if(weaponInfo.attackType == Def.DefEnum.AttackType.ANGLE_RANGE)
+        if (!_weaponInfo.effectPath.Equals(string.Empty))
         {
-            BattleManager.Instance.AngleRangeAttack(transform, weaponInfo.length, weaponInfo.range, GetAttackValue());
+            EffectManager.Instance.LoadEffect(_weaponInfo.effectPath, transform.position, transform.rotation);
         }
-        else if(weaponInfo.attackType == Def.DefEnum.AttackType.FRONT_RANGE)
+
+        if (_weaponInfo.attackType == Def.DefEnum.AttackType.RANGE_EXPLOSION)
         {
             //범위 공격에 맞은 적이 있으면 사운드 출력.
-            if(BattleManager.Instance.FrontRangeAttack(transform, weaponInfo.range, weaponInfo.length, GetAttackValue()))
+            if (BattleManager.Instance.CircleRangeAttack(transform, _weaponInfo.range, GetAttackValue()))
             {
-                SoundManager.Instance.PlaySound(weaponInfo.hitSFXPath);
+                SoundManager.Instance.PlaySound(_weaponInfo.hitSFXPath);
             }
         }
+
+        ResourceManager.Instance.ReleaseResource(this.gameObject);
     }
 
     public void Attacked(int atk)
@@ -118,42 +145,7 @@ public class Bullet : MonoBehaviour, InteractiveObject
     int GetAttackValue()
     {
         //반올림 계산이 5까지 버려서 5는 올리기 위해 0.1 추가.
-        return weaponInfo.attack + Mathf.RoundToInt((weaponInfo.attackUpg + 0.1f) * weaponLevel);
-    }
-
-    //유지 시간 계산.
-    float GetLifeTime()
-    {
-        float lifeTime = weaponInfo.lifeTime + weaponInfo.lifeTimeUpg * weaponLevel;
-        return lifeTime <= weaponInfo.lifeTimeMax ? lifeTime : weaponInfo.lifeTimeMax;
-    }
-
-    //자신 삭제.
-    public void Destroy()
-    {
-        if(destroyCallback != null)
-        {
-            destroyCallback();
-        }
-
-        if(weaponInfo.effectPath != "")
-        {
-            EffectManager.Instance.LoadEffect(weaponInfo.effectPath, transform.position, transform.rotation);
-        }
-
-        if(weaponInfo.attackType == Def.DefEnum.AttackType.RANGE_EXPLOSION)
-        {
-            //범위 공격에 맞은 적이 있으면 사운드 출력.
-            if(BattleManager.Instance.CircleRangeAttack(transform, weaponInfo.range, GetAttackValue()))
-            {
-                SoundManager.Instance.PlaySound(weaponInfo.hitSFXPath);
-            }
-        }
-
-        //Destroy(gameObject);
-        //ResourceManager.Instance.DestroyObject(weaponInfo.bulletPath, gameObject);
-        //gameObject.SetActive(false);
-        ResourceManager.Instance.ReleaseResource(this.gameObject);
+        return _weaponInfo.attack + Mathf.RoundToInt((_weaponInfo.attackUpg + 0.1f) * _weaponLevel);
     }
 
     //private void OnDrawGizmos()
